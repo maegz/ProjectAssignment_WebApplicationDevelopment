c29676ef40f204f2922f003b8dc36b540540c7b1
maegz <max.sundberg@hotmail.com>
2017-01-17 16:14:03 +0100

    Major changes of API and structure

--- js/main.js
+++ js/main.js
@@ -1,68 +1,67 @@
-
-
-
-// SKAFFA WIKIPEDIA-API för info om spelet
-// "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=" + liName;
-
+// BING key: 6e0a34bcb1b64e449022c55f26f66813
 
 "use strict";
 
 
 $(document).ready(function() {
     $("button").click(function() {
+		$("img").fadeIn();
         var userInput = $("input").val();
-        getUserInput(userInput);
+        showUserInput(userInput);
+    });
+
+    // If the user presses Enter, the button click function activates.
+    $("input").keyup(function(event) {
+        if (event.keyCode == 13) {
+            $("button").click();
+        }
     });
 });
 
 // Get top 10 games connected with the users input.
-function getUserInput(input) {
+function showUserInput(input) {
     $.ajax({
         // Get users input for the API URL to get games.
-        url: "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=*&search=" + input, // The URL to the API.
-        type: 'GET', // The HTTP Method
-        data: {}, // Additional parameters here
+        url: "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=*&search=" + input,
+        type: 'GET',
         dataType: 'json',
         // If it works, each object appends to the list-element with the ID and Name.
         success: function(data) {
-            console.log(data);
-            // Clear the dropdown list each time the user searches for a new game.
-            $("ul").html("");
-            // Add game-id and name to the list.
-            for (var i in data) {
-                $("ul").append("<li id=" + data[i].id +">" + data[i].name + "</li>").hide().slideDown();
-            }
-            $("li").click(function() {
-                $("ul").slideUp();
-                $(".carousel-indicators").html("");
-                $(".carousel-inner").html("");
-                // Get all objects in data-array
+			$("img").fadeOut();
+            if (data.length === 0) {
+                $("ul").html("").append("<li>Sorry, couldn't find anything with that name. Try again...</li>").hide().slideDown();
+                console.log("Sorry, couldn't find anything with that name.");
+            } else {
+                // Clear the dropdown list each time the user searches for a new game.
+                $("ul").html("");
+                // Add game-id and name to the list.
                 for (var i in data) {
-                    // If the arrays id-object is the same as the clicked li-id..
-                    if (data[i].id == this.id) {
-                        $(".mainText").html(data[i].name);
-                        $(".backgroundImage").css("background-image", "url(https://images.igdb.com/igdb/image/upload/" + data[i].screenshots[0].cloudinary_id + ".png)").hide().fadeIn(1000);
-                        // Get all the objects in the screenshots-array. Add them to the carousel.
-                        for (var j in data[i].screenshots) {
-                            if (j == 0) {
-                                $(".carousel-indicators").append("<li data-target='#carousel-generic' data-slide-to=" + j + " class='active'></li>");
-                                $(".carousel-inner").append("<div class='item active'><img src='https://images.igdb.com/igdb/image/upload/t_screenshot_med_2x/" + data[i].screenshots[j].cloudinary_id + ".jpg'><div>");
+                    $("ul").append("<li id=" + data[i].id +">" + data[i].name + "</li>").hide().slideDown();
+                }
+                // Clicking on list-items.
+                $("li").click(function() {
+                    // Hide or erase old information in order to create new.
+                    $("ul").slideUp();
+                    $(".carousel-indicators").html("");
+                    $(".carousel-inner").html("");
+                    $("img").fadeIn();
+                    // Get all objects in data-array
+                    for (var i in data) {
+                        // If the arrays id-object is the same as the clicked li-id..
+                        if (data[i].id == this.id) {
+                            $(".mainText").html("<h2>" + data[i].name + "</h2>").hide().fadeIn(2000);
+                            getImages();
+                            $("img").fadeOut();
+                            if (data[i].summary != undefined) {
+                                $(".infoText").html("<h2>Summary from IGDB:</h2><br>" + data[i].summary).hide().fadeIn(2000);
+                            // If the game doesn't have a summary, use the Bing API for getting the first search site as a link.
                             } else {
-                                $(".carousel-indicators").append("<li data-target='#carousel-generic' data-slide-to=" + j + "></li>");
-                                $(".carousel-inner").append("<div class='item'><img src='https://images.igdb.com/igdb/image/upload/t_screenshot_med_2x/" + data[i].screenshots[j].cloudinary_id + ".jpg'><div>");
+                                getGameInfoIfNotExistingOnIGDB();
                             }
-
-                        }
-                        // If the game doesn't have a summary, print out a different text.
-                        if (data[i].summary === undefined) {
-                            $(".infoText").html("Sorry, the summary of this game doesn't exist on IGDB. :(").hide().slideDown();
-                        } else {
-                            $(".infoText").html(data[i].summary).hide().slideDown();
                         }
                     }
-                }
-            });
-            // getChosenGameImage();
+                });
+            }
         },
         error: function(err) { alert(err); },
         beforeSend: function(xhr) {
@@ -71,67 +70,57 @@ function getUserInput(input) {
     });
 }
 
-
-// Create click-function for each list-object.
-function getChosenGameImage() {
-    $("li").click(function() {
-        var liID = this.id;
-        var liName = this.innerHTML;
-        // Get clicked object for the API URL to get images.
-        $.ajax({
-            url: "https://igdbcom-internet-game-database-v1.p.mashape.com/games/" + this.id + "?fields=screenshots",
-            type: "GET",
-            data: {},
-            dataType: "json",
-            // If it works, get the specific ID (hash) for the image. Append to the div-element.
-            success: function(data) {
-                if (data[0].screenshots !== undefined) {
-                    var imageHash = data[0].screenshots[0].cloudinary_id;
-                    for (var i in data[0].screenshots) {
-                        $(".screenshots").append("<img src='https://images.igdb.com/igdb/image/upload/t_screenshot_med_2x/" + data[0].screenshots[i].cloudinary_id + ".jpg'/>");
-                        // var imageUrl = "https://images.igdb.com/igdb/image/upload/t_screenshot_med_2x/" + imageHash + ".jpg";
-                    }
-                    var backgroundImage = "https://images.igdb.com/igdb/image/upload/" + imageHash + ".png";
-                    $(".mainText").css("padding", "4em").html("<h1>" + liName + "</h1>" /*<img id=img" + liID + " src=" + imageUrl + ">"*/).hide().fadeIn(1500);
-                                                //  The%20Witcher%203:%20Wild%20Hunt
-                    // Change all whitespace to "%20" for the HTML-search.
-                    var infoTextCorrector = liName.replace(/\s/g, "%20");
-                    $(".infoText").html("<a>Click Here!</a>");
-                    var gameInfoTextAll = "https://en.wikipedia.org/w/api.php?format=json&srwhat=nearmatch&action=query&prop=extracts&exintro=&explaintext=&titles=" + infoTextCorrector;
-
-                    $(".infoText").click(function() {
-                        $.ajax({
-                            url: gameInfoTextAll,
-                            type: "GET",
-                            data: {},
-                            dataType: "jsonp",
-                            success: function(data) {
-                                var gameID = data.query.pages;
-                                for (var i in gameID) {
-                                    $(".infoText").html(gameID[i].extract).hide();
-                                    console.log(gameID[i].extract);
-                                }
-                                $(".infoText").slideDown();
-                            },
-                            error: function(err) { alert(err); }
-                        });
-                    });
-
-
-                    $("ul").slideUp().empty();
-                    // $(".mainText").append("<h1>" + data[0].name + "</h1><img id=img" + liID + " src=" + imageUrl + ">").hide().fadeIn(1500);
-                    $(".backgroundImage").css("background-image", "url(" + backgroundImage + ")").hide().fadeIn(1000);
-                    // If the ID doesn't contain images, print it out for the user.
+function getImages() {
+    $.ajax({
+        url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + $(".mainText").text() + "+gameplay&count=10&aspect=wide&size=large",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+            $(".backgroundImage").css("background-image", "url(" + data.value[0].contentUrl + ")").hide().fadeIn(2000);
+            $("#carousel-generic").hide().fadeIn(2000);
+            console.log(data.value);
+            for (var i in data.value) {
+                if (i == 0) {
+                    $(".carousel-indicators").append("<li data-target='#carousel-generic' data-slide-to=" + i + " class='active'></li>");
+                    $(".carousel-inner").append("<div class='item active'><img src=" + data.value[i].contentUrl + "><div>").hide().fadeIn(2000);
                 } else {
-                    $(".mainText").css("padding", "5em").html("<p><br>Sorry, no screenshots available!</p>").hide().fadeIn(1500);
+                    $(".carousel-indicators").append("<li data-target='#carousel-generic' data-slide-to=" + i + "></li>");
+                    $(".carousel-inner").append("<div class='item'><img src=" + data.value[i].contentUrl + "><div>");
                 }
-            },
-            error: function(err) { alert(err); },
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "UMH89mEtExmsh2p87keymPsDcroyp10qerijsnbcCdUmwMetfX"); // Enter here your Mashape key
             }
-        });
-    });
+        },
+        error: function(err) { console.log(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Ocp-Apim-Subscription-Key", "6e0a34bcb1b64e449022c55f26f66813"); // Enter here your Mashape key
+        }
+    })
 }
+
+function getGameInfoIfNotExistingOnIGDB() {
+    $.ajax({
+        url: "https://api.cognitive.microsoft.com/bing/v5.0/search?q=" + $(".mainText").text() + "+wikipedia&mkt=en-us",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            console.log(data.webPages.value[0].url);
+            $(".infoText").html("<h2>Sorry, the summary of this game doesn't exist on IGDB.</h2><br><a id='infoLink' href=" + data.webPages.value[0].url + ">Click here for more info on another page</a>").hide().fadeIn(3000);
+            // If the user wants to be redirected to another page, the user must first accept it in a popup-window.
+            $("#infoLink").click(function() {
+                console.log("hej");
+                window.onbeforeunload = function() {
+                    window.onbeforeunload = null;
+                    return "Bye";
+                };
+            });
+        },
+        error: function(err) { console.log(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Ocp-Apim-Subscription-Key", "eab7bca2c7ec405eb83dcb524eb3cc76");
+        }
+    })
+}
+
+
 // api_key=UMH89mEtExmsh2p87keymPsDcroyp10qerijsnbcCdUmwMetfX
 // $.getJSON("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10&offset=0&order=release_dates.date%3Adesc&search=zelda",
